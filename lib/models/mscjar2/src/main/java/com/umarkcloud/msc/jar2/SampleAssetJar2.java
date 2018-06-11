85253c9551bee14717926b884349daf5975201d3
yunjie <13816677222@163.com>
2018-06-11 09:32:55 +0800

    feat: first commit first commit footer

---
+++ lib/models/mscjar2/src/main/java/com/umarkcloud/msc/jar2/SampleAssetJar2.java
@@ -0,0 +1,25 @@
+/*
+ * Licensed Materials - Property of UMARK
+ * THIS MODULE IS "RESTRICTED MATERIALS OF UMARK"
+ * (c) Copyright UMARK Inc. 2018 All Rights Reserved
+ *
+ * The source code for this program is not published or
+ * otherwise divested of its trade secrets
+ */
+
+package com.umarkcloud.msc.jar2;
+
+import com.umarkcloud.oboe.model.User;
+import org.hyperledger.composer.annotation.Asset;
+import org.hyperledger.composer.annotation.DataField;
+import org.hyperledger.composer.annotation.Pointer;
+
+@Asset
+public class SampleAssetJar2 {
+
+  @DataField(primary = true)
+  public String id;
+
+  @Pointer
+  public User user;
+}
---
+++ test/multi_instance.js
@@ -0,0 +1,17 @@
+/*
+ * Licensed Materials - Property of UMARK
+ * THIS MODULE IS "RESTRICTED MATERIALS OF UMARK"
+ * (c) Copyright UMARK Inc. 2018 All Rights Reserved
+ *
+ * The source code for this program is not published or
+ * otherwise divested of its trade secrets
+ */
+
+
+module.exports = {
+  roles: ['seller', 'financingInstitution'],
+  steps: [{
+    role: 'seller',
+    task: 'ProcessRequestSubprocess#Start',
+  }],
+};
